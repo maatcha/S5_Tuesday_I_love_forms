b87c23d6abd1a8e0c28eb92f4831c64a269115b3
maatcha <mattcha54@gmail.com>
2018-07-31 22:54:56 +0200

    pages and routes ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/create'
+  root to: 'pages#home'
+  get '/contact', to: 'pages#contact'
+  get '/new', to: 'users#new'
+  post '/new', to: 'users#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
